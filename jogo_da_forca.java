d97870653c2c7df22da190296d127bc7667adee1
Valdemir Junior <valdemirvoltatoni@gmail.com>
2023-09-28 22:51:00 -0300

    versao_0.1

--- jogo_da_forca.java
+++ jogo_da_forca.java
@@ -12,7 +12,8 @@ public class jogo_da_forca {
         ArrayList <String> paises = new ArrayList<>();
         ArrayList <String> letras_corretas = new ArrayList<>(); // Aqui criamos os arrays para receber as palavras que podem ser sorteadas para a palavra secreta
         ArrayList <String> times = new ArrayList<>();
-
+        ArrayList <String> tema_escolhido = new ArrayList<>();
+        ArrayList <String> comidas = new ArrayList<>();
         Scanner input = new Scanner(System.in);
         Random random = new Random();
 
@@ -24,39 +25,51 @@ public class jogo_da_forca {
         times.add("Flamengo");
         times.add("São Paulo");
         times.add("Corinthians");
+        comidas.add("Macarrão");
+        comidas.add("Pizza");
+        
+        System.out.println("Digite a opção de tema desejado: \n 1 - Países \n 2 - Times \n 3 - Comidas");
+        String tema = input.nextLine();
 
-        //PRECISO FAZER UMA FUNÇÃO PARA PODER ESCOLHER ENTRE OS TEMAS, E DEPOIS DE SELECIONAR O TEMA ESCOLHIDO RANDOMIZAR A PALAVRA DO TEMA
+        switch(tema){
+            case "1":
+                tema_escolhido = paises;
+                break;
+            case "2":
+                tema_escolhido = times;
+                break;
+            case "3":
+                tema_escolhido = comidas;
+                break;
+        }
 
-        int num = random.nextInt(paises.size()); // Aqui sorteamos uma palavra secreta pelo tamanho do array
-        //System.out.println(paises.toString());
-        String pais_escolhido = paises.get(num).toLowerCase(); // Aqui pegamos a palavra secreta pelo indice sorteado do array
-        //System.out.println(pais_escolhido);
-        String palavra_escolhida = pais_escolhido;
+        int num = random.nextInt(tema_escolhido.size()); // Aqui sorteamos uma palavra secreta pelo tamanho do array
+        String escolhe_palavra = tema_escolhido.get(num).toLowerCase(); // Aqui pegamos a palavra secreta pelo indice sorteado do array
+        String palavra_escolhida = escolhe_palavra;
         int tamanho_palavra = palavra_escolhida.length();
             
         char asterisco = '*';
         int chances = 0;
 
-        if (tamanho_palavra > 0 && tamanho_palavra < 5){
+        if (tamanho_palavra > 0 && tamanho_palavra <= 5){
             chances = 5;
-        }else if(tamanho_palavra > 5 && tamanho_palavra < 8){
+        }else if(tamanho_palavra > 5 && tamanho_palavra <= 8){
             chances = 7;
-        }else if(tamanho_palavra > 8 && tamanho_palavra < 12){ // Aqui definimos quantas chances o jogador terá pelo tamanho da palavra
+        }else if(tamanho_palavra > 8 && tamanho_palavra <= 12){ // Aqui definimos quantas chances o jogador terá pelo tamanho da palavra
             chances = 9;
-        }else if(tamanho_palavra > 12 && tamanho_palavra < 16){
+        }else if(tamanho_palavra > 12 && tamanho_palavra <= 16){
             chances = 12;
         }else if(tamanho_palavra > 16){
             chances = 15;
         }
 
-        StringBuilder string = new StringBuilder(pais_escolhido);
+        StringBuilder string = new StringBuilder(palavra_escolhida);
 
-        for (int index = 0 ; index < pais_escolhido.length() ; index ++){
+        for (int index = 0 ; index < palavra_escolhida.length() ; index ++){
             string.setCharAt(index,asterisco); // Aqui criamos uma string para fazer os asteriscos correspondentes pelo tamanho da palavra
         }
-        System.out.println("A palavra secreta é " + string);
         
-        System.out.println("");
+        System.out.println();
         String entrada = "";
         System.out.printf("A palavra secreta tem %d letras e você tem %d chances: ",tamanho_palavra,chances);
         System.out.println();
@@ -71,17 +84,17 @@ public class jogo_da_forca {
         char letra0 = letra.charAt(0);
 
 
-        for (int a = 0 ; a < pais_escolhido.length() ; a++){
-            if (letra0 == pais_escolhido.charAt(a)){ // Aqui fazemos um loop para percorrer toda a palavra e se a letra digitada estiver em alguma posição ela
+        for (int a = 0 ; a < palavra_escolhida.length() ; a++){
+            if (letra0 == palavra_escolhida.charAt(a)){ // Aqui fazemos um loop para percorrer toda a palavra e se a letra digitada estiver em alguma posição ela
                 string.setCharAt(a,letra0);         // é colocada no lugar do asterisco na string
             }
         }  
           
-        if (pais_escolhido.contains(letra)){
-            int count=0;
-            for(int o =0;o<palavra_escolhida.length();o++){
-                if(palavra_escolhida.charAt(o)==letra0){   
-                count++; // Aqui fazemos um loop para contar as ocorrências de cada letra, se tiver mais de uma ocorrências ela são adicionadas proporcionalmente
+        if (palavra_escolhida.contains(letra)){
+            int count = 0;
+            for(int o = 0;o < palavra_escolhida.length();o ++){
+                if(palavra_escolhida.charAt(o) == letra0){   
+                count ++; // Aqui fazemos um loop para contar as ocorrências de cada letra, se tiver mais de uma ocorrências ela são adicionadas proporcionalmente
                 }       // no array de letras corretas para coincidir com o tamanho da palavra e aparecer a mensagem de venceu o jogo
         }
         int letra1 = count;
@@ -93,9 +106,8 @@ public class jogo_da_forca {
         }
     }    
 
-        if (pais_escolhido.contains(letra)){
+        if (palavra_escolhida.contains(letra)){
             System.out.println(string); // Se a letra digitada estiver na palavra secreta a string com a letra em sua respectiva posição é impressa
-            //System.out.println(letras_corretas);
         }else{
             chances--;
             System.out.printf("Essa letra não existe na palavra secreta,você ainda tem %d chances ",chances);
@@ -106,7 +118,7 @@ public class jogo_da_forca {
             break; // Se o tamanho do array de letras corretas for o mesmo do tamanho da palavra secreta o jogo te parabeniza por vencer e encerra!
         }
         if (chances == 0){
-            System.out.printf("Você perdeu,você gastou todas as chances, a palavra secreta era %s",pais_escolhido);
+            System.out.printf("Você perdeu,você gastou todas as chances, a palavra secreta era %s",palavra_escolhida);
             System.out.println(); // Se você perder todas as chances o jogo lamenta que você perdeu e te diz qual era a palavra secreta!
             break;   
         }
--- readme.md
+++ readme.md
@@ -9,3 +9,7 @@ O código já está com uma boa parte encaminhada por quê quis deixar ele funci
 E agora só vou adicionando as modificações que eu for adicionando!
 
 Obrigado por estar aqui e caso tenha alguma sugestão pode me dizer,ficarei muito grato!
+
+Versão 0.1:
+Nessa versão adicionei uma função para poder escolher o tema que deseja para a palavra secreta,algo simples mas que dá mais dinâmica no jogo.
+Espero que gostem!
