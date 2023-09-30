dda6b20ce2c6ca5c80fbf343f09be75a418788ae
Valdemir Junior <valdemirvoltatoni@gmail.com>
2023-09-29 22:51:43 -0300

    versao_0.2

--- jogo_da_forca.java
+++ jogo_da_forca.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-// PRECISO CRIAR UM MECANISMO DE AUTO COMPLETE PARA PALAVRAS COM ACENTO!
 public class jogo_da_forca {
     
     public static void main(String[] args){
@@ -18,15 +17,23 @@ public class jogo_da_forca {
         Random random = new Random();
 
         paises.add("Brasil");
-        paises.add("Russia");
+        paises.add("Rússia");
         paises.add("Argentina");
-        paises.add("Italia");
-        paises.add("Afeganistao");
+        paises.add("Itália");
+        paises.add("Afeganistão");
+        paises.add("Costa Rica");
+        paises.add("Malásia");
+        paises.add("Estados Unidos da América");
         times.add("Flamengo");
         times.add("São Paulo");
         times.add("Corinthians");
+        times.add("Internacional");
+        times.add("Vasco da Gama");
         comidas.add("Macarrão");
         comidas.add("Pizza");
+        comidas.add("Carne");
+        comidas.add("Arroz");
+        comidas.add("Feijão");
         
         System.out.println("Digite a opção de tema desejado: \n 1 - Países \n 2 - Times \n 3 - Comidas");
         String tema = input.nextLine();
@@ -47,7 +54,35 @@ public class jogo_da_forca {
         String escolhe_palavra = tema_escolhido.get(num).toLowerCase(); // Aqui pegamos a palavra secreta pelo indice sorteado do array
         String palavra_escolhida = escolhe_palavra;
         int tamanho_palavra = palavra_escolhida.length();
-            
+        
+        char acento0 = 'ã';
+        char acento1 = 'á';
+        char acento2 = 'â';
+        char acento3 = 'é';
+        char acento4 = 'ê';
+        char acento5 = 'í';
+        char acento6 = 'î';
+        char acento7 = 'ú';
+        char acento8 = 'û';
+        char acento9 = 'ô';
+        char acento10 = 'ó';
+        char acento11 = 'õ';
+        char acento12 = 'ç';
+        String a0 = "ã";
+        String a1 = "á";
+        String a2 = "â";
+        String a3 = "é";
+        String a4 = "ê";
+        String a5 = "í";
+        String a6 = "î";
+        String a7 = "ú";
+        String a8 = "û";
+        String a9 = "ô";
+        String a10 = "ó";
+        String a11 = "õ";
+        String a12 = "ç";
+        
+        char espaco = ' ';  
         char asterisco = '*';
         int chances = 0;
 
@@ -63,17 +98,28 @@ public class jogo_da_forca {
             chances = 15;
         }
 
+
         StringBuilder string = new StringBuilder(palavra_escolhida);
 
         for (int index = 0 ; index < palavra_escolhida.length() ; index ++){
-            string.setCharAt(index,asterisco); // Aqui criamos uma string para fazer os asteriscos correspondentes pelo tamanho da palavra
+            string.setCharAt(index,asterisco);
+            if(espaco == palavra_escolhida.charAt(index)){
+                string.setCharAt(index, espaco);
+            }; // Aqui criamos uma string para fazer os asteriscos correspondentes pelo tamanho da palavra
         }
         
         System.out.println();
         String entrada = "";
-        System.out.printf("A palavra secreta tem %d letras e você tem %d chances: ",tamanho_palavra,chances);
+        System.out.printf("A palavra secreta tem %d letras e você tem %d chances: %s",tamanho_palavra,chances,string);
         System.out.println();
-        
+        String espaco0 = " ";
+
+        for ( int a = 0 ; a < palavra_escolhida.length() ; a++){
+            if(palavra_escolhida.charAt(a) == espaco){
+                letras_corretas.add(espaco0);
+            }
+        }
+
         while (entrada != "sair"){ // Aqui criamos um loop para poder pedir a digitação de letras até que a palavra seja descoberta ou acabe as chances!
 
             System.out.println("Digite uma letra : ");
@@ -82,29 +128,134 @@ public class jogo_da_forca {
                                                                   // letra minúscula
         
         char letra0 = letra.charAt(0);
-
-
+        
+        if(letra0 == 'a'){
+                if (palavra_escolhida.contains(a0)){
+                    for (int index = 0 ; index < palavra_escolhida.length() ; index ++){
+                        if (palavra_escolhida.charAt(index) == acento0){
+                            string.setCharAt(index,acento0);
+                            letras_corretas.add(a0);
+                        }
+                    }
+                }else if (palavra_escolhida.contains(a1)){
+                    for (int index = 0 ; index < palavra_escolhida.length() ; index ++){
+                        if (palavra_escolhida.charAt(index) == acento1){
+                            string.setCharAt(index, acento1);
+                            letras_corretas.add(a1);
+                        }
+                    }
+                }else if (palavra_escolhida.contains(a2)){
+                    for (int index = 0 ; index < palavra_escolhida.length() ; index ++){
+                        if (palavra_escolhida.charAt(index) == acento2){
+                            string.setCharAt(index, acento2);
+                            letras_corretas.add(a2);
+                        }
+                    }
+                }
+        }else if(letra0 == 'e'){
+                if (palavra_escolhida.contains(a3)){
+                    for (int index = 0 ; index < palavra_escolhida.length() ; index ++){
+                        if (palavra_escolhida.charAt(index) == acento3){
+                            string.setCharAt(index, acento3);
+                            letras_corretas.add(a3);
+                        }
+                    }
+                }else if (palavra_escolhida.contains(a4)){
+                    for (int index = 0 ; index < palavra_escolhida.length() ; index ++){
+                        if (palavra_escolhida.charAt(index) == acento4){
+                            string.setCharAt(index, acento4);
+                            letras_corretas.add(a4);
+                        }
+                    }
+                }
+        }else if(letra0 == 'i'){
+                if (palavra_escolhida.contains(a5)){
+                    for (int index = 0 ; index < palavra_escolhida.length() ; index ++){
+                        if (palavra_escolhida.charAt(index) == acento5){
+                            string.setCharAt(index, acento5);
+                            letras_corretas.add(a5);
+                        }
+                    }
+                }else if (palavra_escolhida.contains(a6)){
+                    for (int index = 0 ; index < palavra_escolhida.length() ; index ++){
+                        if (palavra_escolhida.charAt(index) == acento6){
+                            string.setCharAt(index, acento6);
+                            letras_corretas.add(a6);
+                    }
+                }
+            }
+        }else if(letra0 == 'u'){
+                if (palavra_escolhida.contains(a7)){
+                    for (int index = 0 ; index < palavra_escolhida.length() ; index ++){
+                        if (palavra_escolhida.charAt(index) == acento7){
+                            string.setCharAt(index, acento7);
+                            letras_corretas.add(a7);
+                        }
+                    }
+                }else if (palavra_escolhida.contains(a8)){
+                    for (int index = 0 ; index < palavra_escolhida.length() ; index ++){
+                        if (palavra_escolhida.charAt(index) == acento8){
+                            string.setCharAt(index, acento8);
+                            letras_corretas.add(a8);
+                        }
+                    }
+                }
+        }else if(letra0 == 'o'){
+                if (palavra_escolhida.contains(a9)){
+                    for (int index = 0 ; index < palavra_escolhida.length() ; index ++){
+                        if (palavra_escolhida.charAt(index) == acento9){
+                            string.setCharAt(index, acento9);
+                            letras_corretas.add(a9);
+                        }
+                    }
+                }else if (palavra_escolhida.contains(a10)){
+                    for (int index = 0 ; index < palavra_escolhida.length() ; index ++){
+                        if (palavra_escolhida.charAt(index) == acento10){
+                            string.setCharAt(index, acento10);
+                            letras_corretas.add(a10);
+                        }
+                    }
+                }else if (palavra_escolhida.contains(a11)){
+                    for (int index = 0 ; index < palavra_escolhida.length() ; index ++){
+                        if (palavra_escolhida.charAt(index) == acento11){
+                            string.setCharAt(index, acento11);
+                            letras_corretas.add(a11);
+                        }
+                    }
+                }
+        }else if (letra0 == 'c')
+                if (palavra_escolhida.contains(a12)){
+                    for (int index = 0 ; index < palavra_escolhida.length() ; index ++){
+                        if (palavra_escolhida.charAt(index) == acento12){
+                            string.setCharAt(index, acento12);
+                            letras_corretas.add(a12);
+                        }
+                    }
+                }
+                
         for (int a = 0 ; a < palavra_escolhida.length() ; a++){
             if (letra0 == palavra_escolhida.charAt(a)){ // Aqui fazemos um loop para percorrer toda a palavra e se a letra digitada estiver em alguma posição ela
                 string.setCharAt(a,letra0);         // é colocada no lugar do asterisco na string
             }
         }  
-          
+
+
         if (palavra_escolhida.contains(letra)){
             int count = 0;
             for(int o = 0;o < palavra_escolhida.length();o ++){
                 if(palavra_escolhida.charAt(o) == letra0){   
                 count ++; // Aqui fazemos um loop para contar as ocorrências de cada letra, se tiver mais de uma ocorrências ela são adicionadas proporcionalmente
-                }       // no array de letras corretas para coincidir com o tamanho da palavra e aparecer a mensagem de venceu o jogo
+                }      // no array de letras corretas para coincidir com o tamanho da palavra e aparecer a mensagem de venceu o jogo
         }
         int letra1 = count;
-        //System.out.printf("A letra %s aparece %d vezes",letra0,letra1);
-        //System.out.println();
-
         for(int cont = 0; cont < letra1; cont++){
             letras_corretas.add(letra); // Aqui fazemos a adição das ocorrências das letras no array de letras corretas
         }
-    }    
+        //System.out.printf("A letra %s aparece %d vezes",letra0,letra1);
+        //System.out.println();
+    }
+        char primeira_letra = string.charAt(0);
+        Character.toUpperCase(primeira_letra);
 
         if (palavra_escolhida.contains(letra)){
             System.out.println(string); // Se a letra digitada estiver na palavra secreta a string com a letra em sua respectiva posição é impressa
--- readme.md
+++ readme.md
@@ -11,5 +11,12 @@ E agora só vou adicionando as modificações que eu for adicionando!
 Obrigado por estar aqui e caso tenha alguma sugestão pode me dizer,ficarei muito grato!
 
 Versão 0.1:
+
 Nessa versão adicionei uma função para poder escolher o tema que deseja para a palavra secreta,algo simples mas que dá mais dinâmica no jogo.
+Espero que gostem!
+
+Versão 0.2:
+
+Nessa versão adicionei uma função para auto complete de letras com acento, sem o usuário precisar digitá-las, assim que digitar por exemplo a letra o e a palavra tiver ô, ira completar a letra o e ô ao mesmo tempo,sendo assim mais dinâmico o jogo para o usuário.
+
 Espero que gostem!
